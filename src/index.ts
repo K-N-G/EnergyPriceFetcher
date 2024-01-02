85b2f7653ee6fb10b5f5b9830a9cfb0c6ab1a3c7
KNG <rrr_ood@yahoo.com>
2024-01-02 14:52:10 +0200

    refactoring DB url env

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbName = process.env.DB_NAME;
-const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+const dbURL = process.env.DB_URL;
+
+const connectionString = `${dbURL}`;
 mongoose.connect(connectionString);
 
 app.use(express.json());
